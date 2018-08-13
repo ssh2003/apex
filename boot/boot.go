38159b0fe92835e4ffa8cae227acf59aeff6d94c
Serhii Khylyk <serhii.khylyk@glomex.com>
2018-08-13 15:02:00 +0300

    Check

--- boot/boot.go
+++ boot/boot.go
@@ -113,6 +113,7 @@ func (b *Bootstrapper) isProject() bool {
 func (b *Bootstrapper) isUpdate() bool {
 	fmt.Println("isUpdate")
 	fmt.Println(reflect.TypeOf(os.Args))
+	fmt.Println(LastIndexAny(os.Args, "update"))
 	//if indexOf(os.Args, "update") > -1 {
 	//	return true
 	//} else {
