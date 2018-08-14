cb131826a9d03b70b7ed8bdf16b9568bfbd2a8d0
Serhii Khylyk <serhii.khylyk@glomex.com>
2018-08-14 09:34:38 +0300

    Change create role

--- cmd/apex/createrole/createrole.go
+++ cmd/apex/createrole/createrole.go
@@ -9,13 +9,10 @@ import (
 	"github.com/ssh2003/apex/cmd/apex/root"
 )
 
-// Version of program.
-const Version = "1.0.0-ssh2003-alpha0"
-
 // Command config.
 var Command = &cobra.Command{
 	Use:              "createrole",
-	Short:            "Create role in past",
+	Short:            "Create base role for lambda function",
 	PersistentPreRun: root.PreRunNoop,
 	Run:              run,
 }
@@ -27,5 +24,5 @@ func init() {
 
 // Run command.
 func run(c *cobra.Command, args []string) {
-	fmt.Printf("Apex version %s\n", Version)
+	fmt.Println("Create Role")
 }
