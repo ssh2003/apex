f8b3f5e02cd9fade15690d5b3c0c26ea3e1e67cb
Serhii Khylyk <serhii.khylyk@glomex.com>
2018-08-14 11:04:56 +0300

    Role

--- roleinit/roleinit.go
+++ roleinit/roleinit.go
@@ -5,7 +5,7 @@ import (
 	//"io/ioutil"
 	//"os"
 	//"strings"
-	//"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws"
 	//"github.com/aws/aws-sdk-go/service/iam"
 	"github.com/aws/aws-sdk-go/service/iam/iamiface"
 )
@@ -20,5 +20,51 @@ type RoleInit struct {
 
 func (r *RoleInit) RInit() error {
 	fmt.Println("RInit")
+	role, err := r.IAM.ListUsers(&aws.ListUsersInput{
+        MaxItems: aws.Int64(10),
+    })
+	if err != nil {
+	        fmt.Println("Error", err)
+	        return
+	}
 	return nil
 }
+
+/*func (b *Bootstrapper) createRole() (string, error) {
+	roleName := fmt.Sprintf("%s_lambda_function", b.name)
+	policyName := fmt.Sprintf("%s_lambda_logs", b.name)
+
+	logf("creating IAM %s role", roleName)
+	role, err := b.IAM.CreateRole(&iam.CreateRoleInput{
+		RoleName:                 &roleName,
+		AssumeRolePolicyDocument: aws.String(iamAssumeRolePolicy),
+	})
+
+	if err != nil {
+		return "", fmt.Errorf("creating role: %s", err)
+	}
+
+	logf("creating IAM %s policy", policyName)
+	policy, err := b.IAM.CreatePolicy(&iam.CreatePolicyInput{
+		PolicyName:     &policyName,
+		Description:    aws.String("Allow lambda_function to utilize CloudWatchLogs. Created by apex(1)."),
+		PolicyDocument: aws.String(iamLogsPolicy),
+	})
+
+	if err != nil {
+		return "", fmt.Errorf("creating policy: %s", err)
+	}
+
+	logf("attaching policy to lambda_function role.")
+	_, err = b.IAM.AttachRolePolicy(&iam.AttachRolePolicyInput{
+		RoleName:  &roleName,
+		PolicyArn: policy.Policy.Arn,
+	})
+
+	if err != nil {
+		return "", fmt.Errorf("creating policy: %s", err)
+	}
+
+	return *role.Role.Arn, nil
+}
+/*
